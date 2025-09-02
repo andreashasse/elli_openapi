a9174581ce97865b5561392ae48bcd5454364fa3
Andreas Hasselberg <andreas.hasselberg@gmail.com>
2025-09-02 21:28:33 +0200

    Further explorations

--- src/elli_openapi.erl
+++ src/elli_openapi.erl
@@ -1,6 +1,6 @@
 -module(elli_openapi).
 
--export([pelle/0, pelle2/1, test_pelle2/0]).
+-export([pelle/0, to_endpoint/1, test_pelle2/0]).
 
 -include_lib("erldantic/include/erldantic_internal.hrl").
 
@@ -16,19 +16,27 @@ pelle() ->
     erldantic_openapi:endpoints_to_openapi(Endpoints).
 
 test_pelle2() ->
-    pelle2(fun elli_openapi_demo:endpoint/1).
+    Endpoints =
+        [{get, "/pelle", fun elli_openapi_demo:endpoint/1},
+         {post, "/pelle", fun elli_openapi_demo:endpoint2/1}],
+    to_endpoints(Endpoints).
 
-pelle2(Fun) ->
+to_endpoint({HttpMethod, Path, Fun}) ->
     {Module, Function, Arity} = erlang:fun_info_mfa(Fun),
     TypeInfo = erldantic_abstract_code:types_in_module(Module),
     {ok, FunctionSpecs} = erldantic_type_info:get_function(TypeInfo, Function, Arity),
     {RequestBody, ReturnCode, ReturnBody} = join_function_specs(FunctionSpecs),
-    Endpoint0 = erldantic_openapi:endpoint(get, "/pelle"),
+    Endpoint0 = erldantic_openapi:endpoint(HttpMethod, Path),
     Endpoint1 = erldantic_openapi:with_request_body(Endpoint0, Module, RequestBody),
     Endpoint2 =
         erldantic_openapi:with_response(Endpoint1, ReturnCode, "", Module, ReturnBody),
-    Endpoints = [Endpoint2],
-    erldantic_openapi:endpoints_to_openapi(Endpoints).
+    Endpoint2.
+
+to_endpoints(Funs) ->
+    Endpoints = lists:map(fun to_endpoint/1, Funs),
+    {ok, EndpointsJson} = erldantic_openapi:endpoints_to_openapi(Endpoints),
+    Json = json:encode(EndpointsJson),
+    file:write_file("priv/openapi.json", Json).
 
 join_function_specs([#ed_function_spec{args = [Arg],
                                        return =
--- src/elli_openapi_demo.erl
+++ src/elli_openapi_demo.erl
@@ -1,6 +1,14 @@
 -module(elli_openapi_demo).
 
--export([endpoint/1]).
+-export([endpoint/1, endpoint2/1]).
+
+-compile(nowarn_unused_type).
+
+-record(user,
+        {first_name :: string(),
+         last_name :: string(),
+         age :: integer() | undefined,
+         access :: [read | write | delete]}).
 
 -spec endpoint(#{name => string(), shirt_size => small | medium | large}) ->
                   {200, [], #{something => string()}}.
@@ -8,3 +16,7 @@ endpoint(#{name := Name, shirt_size := Size}) ->
     {200,
      [],
      #{something => "Hello " ++ Name ++ ", your shirt size is " ++ atom_to_list(Size) ++ "!"}}.
+
+-spec endpoint2(#user{}) -> {200, [], string()}.
+endpoint2(#user{access = _Access}) ->
+    {200, [], ""}.
